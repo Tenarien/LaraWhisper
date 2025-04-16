fda2c5be16887cdd39a383e938dbcf0dcacdbe03
Tenarien <mamateki7@gmail.com>
2025-04-16 22:49:01 +0100

    feat(route): add routes for login and register methods and pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Home');
 });
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::get('/register', [AuthController::class, 'create'])->name('register');
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'store']);
+});
+
