5080fda5c19d693ffa6ab772aee812a0d90a0387
Tenarien <mamateki7@gmail.com>
2025-04-17 23:27:18 +0100

    feat(resource): add UserResource for user serialisation

--- routes/channels.php
+++ routes/channels.php
@@ -1,3 +1,17 @@
 <?php
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('online', function ($user) {
+    return $user ? new UserResource($user) : null;
+});
+
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, $userId1, $userId2) {
+    return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
+});
+
+Broadcast::channel('message.group.{groupId}', function ($user, $groupId) {
+    return $user->groups->containts('id', $groupId) ? $user : null;
+});
