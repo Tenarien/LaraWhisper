a95682a238fdc88f9013b348402ebc91041998db
Tenarien <mamateki7@gmail.com>
2025-04-17 23:28:21 +0100

    feat(ui): add UserAvatar that display avatar and online status

---
+++ resources/js/Components/Chat/UserAvatar.jsx
@@ -0,0 +1,26 @@
+const UserAvatar = ({ user, online = null, profile = false}) => {
+    let onlineClass = online === true ? "avatar-online" : online === false ? "avatar-offline offline" : "";
+
+    const sizeClass = profile ? "w-40" : "w-8";
+
+    return (
+        <>
+            {user.avatar_url && (
+                <div className={`chat-image avatar ${onlineClass}`}>
+                    <div className={`rounded-full ${sizeClass}`}>
+                        <img src={user.avatar_url} alt={user.name} />
+                    </div>
+                </div>
+            )}
+            {!user.avatar_url && (
+                <div className={`chat-image avatar placeholder ${onlineClass}`}>
+                    <div className={`bg-neutral-400 text-neutral-800 rounded-full ${sizeClass}`}>
+                        <p className="text-xl text-center">{user.name.substring(0, 1)}</p>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default UserAvatar;
