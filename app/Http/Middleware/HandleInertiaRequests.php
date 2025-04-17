f0862ae2ffccd523b08445650da9c75f252d3287
Tenarien <mamateki7@gmail.com>
2025-04-17 23:29:43 +0100

    feat(middleware): share auth user and conversations props via inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Conversation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -35,8 +37,16 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
-            //
-        ]);
+        $user = Auth::user();
+
+        return [
+            ...parent::share($request),
+            'auth' => [
+                'user' => $user,
+            ],
+            'conversations' => $user instanceof \App\Models\User
+                ? Conversation::getConversations($user)
+                : [],
+        ];
     }
 }
