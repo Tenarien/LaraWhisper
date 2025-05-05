0e87ebaa27e6c83161a90e22bf2e66a7e6d5ee85
Tenarien <mamateki7@gmail.com>
2025-05-05 23:14:41 +0100

    feat(ui): add group management UI for group owners

--- resources/js/Components/Chat/ConversationHeader.jsx
+++ resources/js/Components/Chat/ConversationHeader.jsx
@@ -1,10 +1,28 @@
-import { Link, usePage } from "@inertiajs/react";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import {Link, router, usePage} from "@inertiajs/react";
+import {ArrowLeftIcon, PencilSquareIcon, TrashIcon} from "@heroicons/react/24/solid";
 import UserAvatar from "@/Components/Chat/UserAvatar.jsx";
 import GroupAvatar from "@/Components/Chat/GroupAvatar.jsx";
+import GroupDescription from "@/Components/Chat/GroupDescription.jsx";
+import GroupUsers from "@/Components/Chat/GroupUsers.jsx";
+import {useEventBus} from "@/EventBus.jsx";
 
 const ConversationHeader = ({ selectedConversation }) => {
+    const {auth} = usePage().props
+    const {emit} = useEventBus();
 
+    const onDelete = () => {
+        if (!window.confirm("Are you sure you want to delete this group?")) return;
+
+        axios.delete(window.route("group.destroy", selectedConversation.id))
+            .then(({data}) => {
+                emit('toast.show', data.message);
+
+                router.visit(data.redirect_url);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    };
     return (
         <>
             {selectedConversation && (
@@ -29,6 +47,35 @@ const ConversationHeader = ({ selectedConversation }) => {
                             )}
                         </div>
                     </div>
+                    {selectedConversation.is_group && (
+                        <div className="flex gap-3">
+                            <GroupDescription description={selectedConversation.description} />
+                            <GroupUsers users={selectedConversation.users} />
+                            {selectedConversation.owner_id === auth.user.id && (
+                                <div className="flex gap-3">
+                                    <div className="tooltip tooltip-left" data-tip="Edit this Group">
+                                        <button
+                                            onClick={() => {
+                                                emit("group.show.modal", selectedConversation);
+                                            }}
+                                            className="text-neutral-400 hover:text-neutral-200"
+                                        >
+                                            <PencilSquareIcon className="w-6 h-6"/>
+                                        </button>
+                                    </div>
+                                    <div className="tooltip tooltip-left" data-tip="Delete this Group">
+                                        <button
+                                            onClick={onDelete}
+                                            className="text-neutral-400 hover:text-neutral-200"
+                                        >
+                                            <TrashIcon className="w-6 h-6" />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            )}
+                        </div>
+                    )}
                 </div>
             )}
         </>
