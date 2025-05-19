ab6b604c0b6e7fa60bdbd1b519958c3c7617b42c
Tenarien <mamateki7@gmail.com>
2025-05-19 22:30:21 +0100

    feat(model): add organisation support and last message relations

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,21 +2,34 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\OrganisationScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'organisation_id',
         'name',
         'description',
         'owner_id',
         'last_message_id',
     ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new OrganisationScope);
+    }
+
+    public function organisation()
+    {
+        return $this->belongsTo(Organisation::class);
+    }
 
     public function owner() : BelongsTo
     {
@@ -28,25 +41,48 @@ class Group extends Model
         return $this->belongsToMany(User::class, 'group_users');
     }
 
-    public function messages() : BelongsToMany
+    public function messages(): HasMany
     {
-        return $this->belongsToMany(Message::class);
+        return $this->hasMany(Message::class, 'group_id');
+    }
+
+    public function lastMessage() : BelongsTo
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
+    public function getLastMessage(): Model
+    {
+        return $this->messages()
+            ->orderBy('created_at', 'desc')
+            ->first();
+    }
+
+    public function getLastMessageId(): ?int
+    {
+        return optional($this->getLastMessage())->id;
     }
 
     public static function getGroupsExceptUser(User $user)
     {
-        $query = self::select([
+        $orgId = $user->organisation_id;
+
+        if (is_null($orgId)) {
+            return collect();
+        }
+
+        return Group::select([
             'groups.*',
             'messages.message as last_message',
             'messages.created_at as last_message_date',
         ])
+            ->where('groups.organisation_id', $orgId)
             ->join('group_users', 'group_users.group_id', '=', 'groups.id')
-            ->leftJoin('messages', 'messages.id', '=', 'groups.last_message_id')
             ->where('group_users.user_id', $user->id)
+            ->leftJoin('messages', 'messages.id', '=', 'groups.last_message_id')
             ->orderBy('messages.created_at', 'desc')
-            ->orderBy('groups.name');
-
-        return $query->get();
+            ->orderBy('groups.name')
+            ->get();
     }
 
     public function toConversationArray()
