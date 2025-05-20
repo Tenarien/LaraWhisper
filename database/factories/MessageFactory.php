3721df50238da36dc519a75b876cd7800d3c6dc8
Tenarien <mamateki7@gmail.com>
2025-05-20 23:53:19 +0100

    refactor(test): modify the message factory to greatly simplify the logic

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -18,33 +18,20 @@ class MessageFactory extends Factory
      */
     public function definition(): array
     {
-        // Randomly set $senderId as either 0 or 1
-        $senderId = $this->faker->numberBetween(0, 1);
-
-        // If $senderId is 0, set a random user as the sender (excluding user with id 1)
-        if ($senderId === 0) {
-            $senderId = $this->faker->randomElement(User::where('id', '!=', $senderId)->pluck('id')->toArray());
-
-            // Make user with id 1 the receiver (admin)
-            $receiverId = 1;
-        } else {
-            // If $senderId is not 0, set a random user as the receiver
-            $receiverId = $this->faker->randomElement(User::pluck('id')->toArray());
-        }
-
+        $senderId = $this->faker->randomElement(User::pluck('id')->toArray());
+        $receiverId = $this->faker->randomElement(
+            User::where('id', '!=', $senderId)->pluck('id')->toArray()
+        );
         $groupId = null;
 
-        // 50% chance that the message is a part of the group
         if ($this->faker->boolean(50)) {
-            // Select random group from all groups
-            $groupId = $this->faker->randomElement(Group::pluck('id')->toArray());
-
-            $group = Group::find($groupId);
-
-            // Choose a random user from the group as the sender
-            $senderId = $this->faker->randomElement($group->users->pluck('id')->toArray());
-
-            $receiverId = null;
+            $groupsWithUsers = Group::has('users')->get();
+            if ($groupsWithUsers->isNotEmpty()) {
+                $group = $this->faker->randomElement($groupsWithUsers->all());
+                $groupId = $group->id;
+                $senderId = $this->faker->randomElement($group->users->pluck('id')->toArray());
+                $receiverId = null;
+            }
         }
 
         return [
