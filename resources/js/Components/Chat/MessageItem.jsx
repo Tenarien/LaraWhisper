4dde104a48d3fa81fcfbd9590bcf4e2ddca390d9
Tenarien <mamateki7@gmail.com>
2025-05-20 23:56:05 +0100

    feat(ui): implement MessageDropdown component into MessageItem component

--- resources/js/Components/Chat/MessageItem.jsx
+++ resources/js/Components/Chat/MessageItem.jsx
@@ -1,15 +1,21 @@
 import { usePage } from "@inertiajs/react";
 import ReactMarkdown from "react-markdown";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserAvatar from "@/Components/Chat/UserAvatar.jsx";
 import { formatMessageDateLong } from "@/helpers";
 import MessageAttachments from "@/Components/Chat/MessageAttachments.jsx";
+import MessageDropdown from "@/Components/Chat/MessageDropdown.jsx";
 
 const MessageItem = ({ message, attachmentClick }) => {
     const currentUser = usePage().props.auth.user;
+    const [isHidden, setIsHidden] = useState(true);
 
     return (
-        <div className={"chat " + (message.sender_id === currentUser.id ? "chat-end" : "chat-start")}>
+        <div
+            className={"chat " + (message.sender_id === currentUser.id ? "chat-end" : "chat-start")}
+            onMouseOver={(e) => {setIsHidden(false)}}
+            onMouseLeave={(e) => {setIsHidden(true)}}
+        >
             <UserAvatar user={message.sender} />
             <div className="chat-header">
                 {message.sender_id !== currentUser.id ? message.sender.name : ""}
@@ -18,6 +24,9 @@ const MessageItem = ({ message, attachmentClick }) => {
                 </time>
             </div>
             <div className={"chat-bubble relative break-words " + (message.sender_id === currentUser.id ? "bg-blue-500" : "bg-neutral-800")} >
+                {message.sender_id === currentUser.id && (
+                    <MessageDropdown message={message} hide={isHidden}/>
+                )}
                 <div className="chat-message">
                     <div className="chat-message-content">
                         <ReactMarkdown>{message.message}</ReactMarkdown>
