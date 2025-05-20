b63a88dc2d7589dd11cba77088ebac6c25a235e8
Tenarien <mamateki7@gmail.com>
2025-05-20 23:50:32 +0100

    feat(model): add HasFactory

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Models\Scopes\OrganisationScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Conversation extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'organisation_id',
         'user_id1',
