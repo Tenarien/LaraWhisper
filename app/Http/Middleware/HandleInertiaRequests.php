c8e9aad3d55d8b10c225bff6e7eb8195a063ac8e
Tenarien <mamateki7@gmail.com>
2025-05-21 22:47:49 +0100

    feat(inertia): add organisation owner_id to shared props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\Conversation;
+use App\Models\Organisation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
@@ -38,11 +39,23 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         $user = Auth::user();
+        $isOrganisationOwner = null;
+        $org = null;
+        if ($user) {
+            $isOrganisationOwner = $user->organisation && $user->id === $user->organisation->owner_id;
+        }
+
+
+        if($isOrganisationOwner) {
+            $org = Organisation::where('owner_id', $user->organisation->owner_id)->first();
+        }
 
         return [
             ...parent::share($request),
             'auth' => [
                 'user' => $user,
+                'is_organisation_owner' => $isOrganisationOwner,
+                'organisation' => $org
             ],
             'conversations' => $user instanceof \App\Models\User
                 ? Conversation::getConversations($user)
