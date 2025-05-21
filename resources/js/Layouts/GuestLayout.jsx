fce4f2ce86ff0a9752702bc1a522ecf3a3d5d61e
Tenarien <mamateki7@gmail.com>
2025-05-21 22:46:55 +0100

    chore(ui): small visual change to guest layout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,10 +1,11 @@
-
 export default function GuestLayout({ children }) {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="w-full max-w-md p-6">
-                {children}
+        <>
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="w-full p-6">
+                    {children}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
